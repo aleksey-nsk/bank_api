25d879b223919d1310b9addfdcf9f106b1105b63
Жданов Алексей Владимирович <AVlZhdanov@sberbank.ru>
2021-10-01 10:30:38 +0700

    start project

---
+++ src/test/java/com/example/bank_api/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
