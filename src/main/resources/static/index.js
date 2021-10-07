e0efc0c0f2bea1405072dfdc0de3f67934df6896
Жданов Алексей Владимирович <AVlZhdanov@sberbank.ru>
2021-10-07 11:38:17 +0700

    вроде создание карты заработало

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -31,6 +31,55 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
         // $scope.getCards(clientId);
     };
 
+    // $scope.saveProduct = function () {
+    //     $scope.NewProduct.category = $scope.NewCategory;
+    //     console.log($scope.NewProduct);
+    //     $http.post(contextPath + '/product', $scope.NewProduct)
+    //             .then(function (resp) {
+    //                 $scope.fillTable();
+    //             });
+    // };
+    $scope.saveCard = function () {
+        console.log("method saveCard()")
+        console.log($scope.NewAccount);
+        // $scope.NewCard.account = $scope.NewAccount;
+
+        let searchNumber = $scope.NewAccount;
+        console.log(searchNumber);
+        //const found = x.find(entry => entry.id == 1002);
+        let account = $scope.Accounts.find(entry => entry.number == 55555666667777788888);
+        // console.log(account);
+        console.log(account.id);
+
+        console.log($scope.NewCard);
+        let objectA = {};
+        console.log(objectA);
+        const url = contextPath + '/api/v1/client/' + 1 + '/account/' +account.id + '/card';
+        console.log(url);
+        $http.post(url, objectA)
+                .then(function (resp) {
+                    console.log(resp);
+                    $scope.fillPage(1);
+                });
+    };
+
+    // $scope.deleteProduct = function (id) {
+    //     $http.delete(contextPath + '/product/' + id)
+    //             .then(function (resp) {
+    //                 $scope.fillTable();
+    //             });
+    // };
+
+    // $scope.changePrice = function (product, new_price) {
+    //     product.price = new_price;
+    //     console.log(product)
+    //     $http.put(contextPath + '/product/' + product.id, product)
+    //             .then(function (resp) {
+    //                 console.log(resp);
+    //                 $scope.fillTable()
+    //             });
+    // };
+
     $scope.fillPage(1);
 
 });
