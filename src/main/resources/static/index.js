ae8291dc6391093200be18b3356d89c94e164506
Alex Zhd <my_email@test.com>
2021-10-07 07:26:44 +0700

    заработали номера счетов

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,29 +1,72 @@
 // $scope - хранит всё, до чего можно достучаться в html-е;
-// $http - через неё можно делать запросы на наш бэкенд
+// $http - через неё можно делать запросы на бэкенд
 angular.module('app', []).controller('indexController', function ($scope, $http) {
 
     const contextPath = 'http://localhost:8082';
+    console.log(contextPath); // лог в консоль браузера (F12 - Console)
 
-    $scope.getCards = function (clientId) {
-        const url = contextPath + '/api/v1/client/' + clientId + '/card';
-        $http.get(url)
-                .then(function (resp) {
-                    $scope.Cards = resp.data;
-                });
-    };
-
-    $scope.fillPage = function (clientId) {
+    $scope.getAccounts = function (clientId) {
         const url = contextPath + '/api/v1/client/' + clientId + '/account';
         console.log(url);
-
         $http.get(url)
                 .then(function (resp) {
-                    console.log(resp); // лог в консоль браузера (F12 - Console)
                     $scope.Accounts = resp.data;
-                    $scope.getCards(clientId);
+                    console.log($scope.Accounts);
                 });
     };
 
+    // $scope.getCards = function (clientId) {
+    //     const url = contextPath + '/api/v1/client/' + clientId + '/card';
+    //     console.log(url);
+    //     $http.get(url)
+    //             .then(function (resp) {
+    //                 $scope.Cards = resp.data;
+    //                 console.log($scope.Cards);
+    //             });
+    // };
+
+    // $scope.getCards = function (clientId) {
+    //     const url = contextPath + '/api/v1/client/' + clientId + '/card';
+    //     console.log(url);
+    //     $http.get(url)
+    //             .then(function (resp) {
+    //                 $scope.Cards = resp.data;
+    //                 console.log($scope.Cards);
+    //
+    //                 console.log("ПОПРОБУЮ ДОБАВИТЬ счета");
+    //                 $scope.Cards.forEach(function (item, index, arr) {
+    //                     console.log(item, index, arr);
+    //                     item['accountNumber'] = '2222222222';
+    //                 });
+    //
+    //                 console.log("опять карты");
+    //                 console.log($scope.Cards);
+    //             });
+    // };
+
+    // $scope.accountNumberByCard = function (card) {
+    //     console.log("Ищу номер счета по карте");
+    //     console.log(card);
+    //     console.log($scope.Accounts);
+    //
+    //     $scope.Accounts.forEach(function (element) {
+    //         console.log(element)
+    //
+    //         // let num = element.number;
+    //         // let c = element.cards;
+    //         // if (c.filter(e => e.number === card.number).length > 0) {
+    //         //     return num;
+    //         // }
+    //     });
+    // };
+
+    $scope.fillPage = function (clientId) {
+        console.log("Получить данные для заполнения страницы")
+        $scope.getAccounts(clientId);
+        // $scope.getCards(clientId);
+    };
+
+    console.log("PRIVET !!!!!!!!!!");
     $scope.fillPage(1);
 
 });
