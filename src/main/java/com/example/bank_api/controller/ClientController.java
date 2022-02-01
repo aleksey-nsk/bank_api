65041a877ca6656f718ab067be5dc4d5560c80f5
Alex Zhd <my_email@test.com>
2022-02-01 14:00:31 +0700

    добавил swagger. Прописал пока только для контроллера клиентов

--- src/main/java/com/example/bank_api/controller/ClientController.java
+++ src/main/java/com/example/bank_api/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.example.bank_api.controller;
 
 import com.example.bank_api.dto.ClientDto;
 import com.example.bank_api.service.ClientService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/client")
-//@Api(description = "Контроллер для клиентов")
+@Api(description = "Контроллер для клиентов")
 public class ClientController {
 
     private final ClientService clientService;
@@ -22,35 +24,35 @@ public class ClientController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-//    @ApiOperation(value = "Получить всех клиентов")
+    @ApiOperation(value = "Получить всех клиентов")
     public List<ClientDto> findAll() {
         return clientService.findAll();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-//    @ApiOperation(value = "Получить клиента по id")
+    @ApiOperation(value = "Получить клиента по id")
     public ClientDto findById(@PathVariable("id") Long id) {
         return clientService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-//    @ApiOperation(value = "Добавить клиента")
+    @ApiOperation(value = "Добавить клиента")
     public ClientDto save(@RequestBody ClientDto clientDto) {
         return clientService.save(clientDto);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-//    @ApiOperation(value = "Обновить клиента")
+    @ApiOperation(value = "Обновить клиента")
     public void update(@PathVariable("id") Long id, @RequestBody ClientDto clientDto) {
         clientService.update(id, clientDto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @ApiOperation(value = "Удалить клиента")
+    @ApiOperation(value = "Удалить клиента")
     public void delete(@PathVariable("id") Long id) {
         clientService.delete(id);
     }
