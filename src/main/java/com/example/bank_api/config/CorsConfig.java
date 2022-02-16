f6059fac914af70e6d0f1d87b440615d0aa22261
Alex Zhd <my_email@test.com>
2022-02-16 11:55:12 +0700

    readme update

--- src/main/java/com/example/bank_api/config/CorsConfig.java
+++ src/main/java/com/example/bank_api/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package com.example.bank_api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("prod")
 public class CorsConfig implements WebMvcConfigurer {
 
     // Настройка CORS (Cross-Origin Resource Sharing)
